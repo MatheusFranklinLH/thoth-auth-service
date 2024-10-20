9859f23dd9b492a1490981f5a840b43dc898f037
Matheus Franklin <matheus.franklin@vitauautomation.com>
2024-10-20 10:34:21 -0300

    Add Permission Requests Tests

---
+++ Thoth.Tests/Requests/Permission/UpdatePermissionRequestTests.cs
@@ -0,0 +1,61 @@
+using Thoth.API.Requests;
+using Xunit;
+
+namespace Thoth.Tests.Requests {
+	public class UpdatePermissionRequestTests {
+		[Fact]
+		public void Should_Return_Valid_When_Data_Is_Correct() {
+			var request = new UpdatePermissionRequest {
+				Id = 1,
+				Name = "Valid Permission"
+			};
+
+			request.Validate();
+
+			Assert.True(request.IsValid);
+			Assert.Empty(request.Notifications);
+		}
+
+		[Fact]
+		public void Should_Return_Invalid_When_Id_Is_Zero() {
+			var request = new UpdatePermissionRequest {
+				Id = 0,
+				Name = "Valid Name"
+			};
+
+			request.Validate();
+
+			Assert.False(request.IsValid);
+			Assert.NotEmpty(request.Notifications);
+			Assert.Contains(request.Notifications, n => n.Key == "Id");
+		}
+
+		[Fact]
+		public void Should_Return_Invalid_When_Name_Is_Empty() {
+			var request = new UpdatePermissionRequest {
+				Id = 1,
+				Name = ""
+			};
+
+			request.Validate();
+
+			Assert.False(request.IsValid);
+			Assert.NotEmpty(request.Notifications);
+			Assert.Contains(request.Notifications, n => n.Key == "Name");
+		}
+
+		[Fact]
+		public void Should_Return_Invalid_When_Name_Is_Too_Short() {
+			var request = new UpdatePermissionRequest {
+				Id = 1,
+				Name = "A"
+			};
+
+			request.Validate();
+
+			Assert.False(request.IsValid);
+			Assert.NotEmpty(request.Notifications);
+			Assert.Contains(request.Notifications, n => n.Key == "Name");
+		}
+	}
+}
